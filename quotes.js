c968fec76b07c61832014e32df236422c2824cea
Cherie Woo <cheriejawoo@yahoo.com>
2020-06-24 12:49:33 -0700

    Adding a quotes file to share quotes
    Quotes I find interesting... or things I read that I like.

---
+++ quotes.js
@@ -0,0 +1,6 @@
+[
+  {
+    "quote": "The web was originally conceived and developed to meet the demand for automated information-sharing between scientists in universities and institutes around the world.",
+    "reason": "This quote is telling of the environment that birthed the internet."
+  }
+]
